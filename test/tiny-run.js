c8537534885f4be5fe0bcd0788e37e78260bd468
unscriptable <john@unscriptable.com>
2012-11-22 11:15:54 -0500

    create test run.js file

---
+++ test/tiny-run.js
@@ -0,0 +1,30 @@
+(function () {
+
+	var config = {
+		baseUrl: '',
+		destUrl: 'test/output/built.js',
+		paths: {
+			"test-js": 'test'
+		},
+		pluginPath: 'curl/plugin'
+	};
+
+	curl(config, ['test-js/tiny']).then(
+		function () {
+			setPageState('loaded');
+			// this should never get called:
+			curl(['some/other/module']);
+		},
+		function () {
+			setPageState('failed');
+		}
+	);
+
+	// do something DOM-ish (this code should never execute during build!)
+	function setPageState (stateClass) {
+		var root = document.documentElement;
+		root.className = root.className.replace(/loading/, '')
+			+ ' ' + stateClass;
+	}
+
+}());
