7ee92e11c07e9ca3d801fbe305d99c178d8d68e0
unscriptable <john@unscriptable.com>
2012-12-11 23:59:08 -0500

    added module dep to wire spec

--- test/data/spec.js
+++ test/data/spec.js
@@ -1,10 +1,12 @@
-define({
+define([], {
 
 	component: {
-		literal: {}
+		module: 'test/depA'
 	},
 
 	plugins: [
 		//{ module: 'wire/debug' }
 	]
 });
+// if run from cram folder:
+// node cram test/data/wire-run.js -r . -o test/data/wire-bundle.js
