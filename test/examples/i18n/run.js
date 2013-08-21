a507770a8f6cd43886cb972b1b7b2aa94bfc06d1
unscriptable <john@unscriptable.com>
2013-08-21 15:58:14 -0400

    Move examples folder to subdir of test folder.

--- test/examples/i18n/run.js
+++ test/examples/i18n/run.js
@@ -1,5 +1,6 @@
 curl.config({
 	packages: {
+		curl: { location: '../../../amd_modules/curl/src/curl' },
 		app: { location: 'app', config: { moduleLoader: 'curl/loader/cjsm11' } }
 	},
 	main: 'app/main',
