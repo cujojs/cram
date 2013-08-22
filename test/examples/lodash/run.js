b28dd6bd3e7b4b39968250c0b67bc7a79c230d54
unscriptable <john@unscriptable.com>
2013-08-22 19:53:55 -0400

    Don't let double backslashes look like escaped quotes. Use lodash.compat.min as even better test.

--- test/examples/lodash/run.js
+++ test/examples/lodash/run.js
@@ -1,8 +1,8 @@
 curl.config({
 	packages: {
 		curl: { location: '../../../amd_modules/curl/src/curl' },
-		underscore: { location: 'amd_modules/lodash', main: 'lodash' }
+		underscore: { location: 'amd_modules/lodash/dist', main: 'lodash.compat.min' }
 	}
 });
 
-curl(['underscore']).then(function () { console.log('loaded'); });
+curl(['underscore']).then(function (_) { console.log('loaded', _.all); });
